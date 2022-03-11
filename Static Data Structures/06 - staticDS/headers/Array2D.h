d93aad14aadd3216a60c4f5ecf82d3383ed7ead0
enxel <earaujo@uca.edu.sv>
2022-03-11 11:53:17 -0600

    Static Data Structures v1.8

---
+++ Static Data Structures/06 - staticDS/headers/Array2D.h
@@ -0,0 +1,71 @@
+template <typename type>
+class Array2D: public Array<type>{
+    private:
+        type** matrix;
+        int nrows, ncols;
+    public:
+        ndimensions category() override {
+            return TWO_DIMENSIONS;
+        }
+        void create(Data dim) override {
+            nrows = dim.n;
+            ncols = dim.m;
+            matrix = (type**) malloc(sizeof(type*)*nrows);
+            for(int i = 0; i < nrows; i++)
+                *(matrix+i) = (type*) malloc(sizeof(type)*ncols);
+        }
+        Data getDim() override {
+            Data dim;
+            dim.n = nrows; dim.m = ncols;
+            return dim;
+        }
+        void init() override {
+            for(int i = 0; i < nrows*ncols; i++)
+                matrix[i/ncols][i%ncols] = (type) 0;
+        }
+        void insert(Data pos, type value) override {
+            matrix[pos.n][pos.m] = value;
+        }
+        type extract(Data pos) override {
+            return matrix[pos.n][pos.m];
+        }
+        void destroy() override {
+            for(int i = 0; i < nrows; i++)
+                free(*(matrix+i));
+            free(matrix);
+        }
+        bool search(type value) override {
+            bool ans = false;
+            for(int i = 0; i < nrows*ncols; i++)
+                if(matrix[i/ncols][i%ncols] == value){
+                    ans = true;
+                    break;
+                }
+            return ans;
+        }
+        int count(type value) override {
+            int cont = 0;
+            for(int i = 0; i < nrows*ncols; i++)
+                if(matrix[i/ncols][i%ncols] == value)
+                    cont++;
+            return cont;
+        }
+        void reverse() override {
+            cout << "->**WARNING: Operation not applicable to a two-dimensional array** :).\n";
+        }
+        void show(bool verbose) override {
+            if(verbose)
+                for(int i = 0; i < nrows*ncols; i++)
+                    cout << "Element in cell [ " << i/ncols+1 << ", " << i%ncols+1 << " ] is: " << matrix[i/ncols][i%ncols] << "\n";
+            else{
+                cout << "[\n";
+                for(int i = 0; i < nrows; i++){
+                    cout << "[ ";
+                    for(int j = 0; j < ncols-1; j++)
+                        cout << matrix[i][j] << ", ";
+                    cout << matrix[i][ncols-1] << " ]\n";
+                }
+                cout << "]\n";
+            }
+        }
+};
---
+++ Static Data Structures/06 - staticDS/headers/staticDS.h
@@ -0,0 +1,108 @@
+enum ndimensions {ONE_DIMENSION, TWO_DIMENSIONS};
+
+typedef struct Data{
+    int n;
+    int m;
+    Data(){};
+} Data;
+
+#include "Array.h"
+#include "Array1D.h"
+#include "Array2D.h"
+
+template <typename type>
+class Arrays{
+    private:
+        static Data ref;
+
+        Arrays(){}
+    public:
+        static void create(Array<type>** array, int size){
+            ref.n = size;
+            *array = new Array1D<type>();
+            (*array)->create(ref);
+        }
+        static void create(Array<type>** matrix, int nrows, int ncols){
+            ref.n = nrows; ref.m = ncols;
+            *matrix = new Array2D<type>();
+            (*matrix)->create(ref);
+        }
+
+        static void getDim(Array<type>* array, int* size){
+            ref = array->getDim();
+            *size = ref.n;
+        }
+        static void getDim(Array<type>* matrix, int* rows, int* columns){
+            ref = matrix->getDim();
+            *rows = ref.n;
+            *columns = ref.m;
+        }
+
+        static void init(Array<type>* var){
+            var->init();
+        }
+
+        static void insert(Array<type>* array, int pos, type value){
+            ref.n = pos;
+            array->insert(ref,value);
+        }
+        static void insert(Array<type>* matrix, int row, int column, type value){
+            ref.n = row; ref.m = column;
+            matrix->insert(ref,value);
+        }
+
+        static void extract(Array<type>* array, int pos, type* result){
+            ref.n = pos;
+            *result = array->extract(ref);
+        }
+        static void extract(Array<type>* matrix, int row, int column, type* result){
+            ref.n = row; ref.m = column;
+            *result = matrix->extract(ref);
+        }
+
+        static void destroy(Array<type>* var){
+            var->destroy();
+        }
+
+        static void search(Array<type>* var, type value, bool* result){
+            *result = var->search(value);
+        }
+
+        static void count(Array<type>* var, type value, int* result){
+            *result = var->count(value);
+        }
+
+        static void reverse(Array<type>* var){
+            var->reverse();
+        }
+
+        static void show(Array<type>* var, bool verbose){
+            var->show(verbose);
+        }
+
+        //Utilidades externas a la familia de clases 'Array'
+        static void create_copy(Array<type>* original, Array<type>** clone){
+            Data dim;
+            switch(original->category()){
+                case ONE_DIMENSION:
+                    *clone = new Array1D<type>();
+                    dim = original->getDim();
+                    (*clone)->create(dim);
+                    for(int i = 0; i < dim.n; i++){
+                        ref.n = i;
+                        (*clone)->insert(ref, original->extract(ref));
+                    }
+                    break;
+                case TWO_DIMENSIONS:
+                    *clone = new Array2D<type>();
+                    dim = original->getDim();
+                    (*clone)->create(dim);
+                    for(int i = 0; i < dim.n*dim.m; i++){
+                        ref.n = i/dim.m;
+                        ref.m = i%dim.m;
+                        (*clone)->insert(ref, original->extract(ref));
+                    }
+                    break;
+            }
+        }
+};
