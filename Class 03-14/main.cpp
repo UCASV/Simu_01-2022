ed162bc4ed63304a628a2299a659f356a37dae7e
enxel <earaujo@uca.edu.sv>
2022-03-11 08:43:02 -0600

    Clase 14-marzo v1.3

--- Class 03-14/main.cpp
+++ Class 03-14/main.cpp
@@ -1,11 +1,16 @@
-#include "array_i.h"
+//#include "array_i.h"
 //#include "array_c.h"
 //#include "array.h"
 //#include "matrix.h"
 //#include "matrix_v2.h"
+#include "static.h"
+
+template<>
+Data Arrays<int>::ref = Data();
 
 int main(void){
-
+//********** Primer bloque de pruebas: arreglos de enteros **********/
+/*
     int n = 10;
     int* array = create(n);
     int* array2;
@@ -25,7 +30,8 @@ int main(void){
     cout << count(array2, n, 3) << "\n";
     array = reverse(array, n);
     cout << array[0] << " - " << array[1] << " - " << array[2] << "\n";
-
+*/
+//********** Segundo bloque de pruebas: arreglos de caracteres **********/
 /*
     int n = 10;
     char* array = create(n);
@@ -47,6 +53,7 @@ int main(void){
     array = reverse(array, n);
     cout << array[0] << " - " << array[1] << " - " << array[2] << "\n";
 */
+//********** Tercer bloque de pruebas: arreglos de cualquier tipo **********/
 /*
     int n = 10;
     int* array = create<int>(n);
@@ -87,6 +94,7 @@ int main(void){
     array3 = reverse<char>(array3, n);
     cout << array3[0] << " - " << array3[1] << " - " << array3[2] << "\n";
 */
+//********** Cuarto bloque de pruebas: matrices de cualquier tipo **********/
 /*
     int filas = 3, columnas = 4;
     int** matrix = create<int>(filas, columnas);
@@ -107,5 +115,52 @@ int main(void){
     matrix2 = create_copy<int>(matrix, filas, columnas);
     cout << count<int>(matrix2, filas, columnas, 3) << "\n";
 */
+//********** Quinto bloque de pruebas: clase administradora de arreglos estáticos **********/
+
+    int n = 5, f = 3, c = 4, val;
+    int n2, f2, c2, cont;
+    bool bval;
+
+    Array<int>* array;
+    Arrays<int>::create(&array, n);
+    Arrays<int>::insert(array,2,5);
+    Arrays<int>::extract(array,2,&val);
+    cout << val << "\n";
+    Arrays<int>::destroy(array);
+    Arrays<int>::create(&array,n+3);
+    Arrays<int>::insert(array,5,15);
+    Arrays<int>::extract(array,5,&val);
+    cout << val << "\n";
+    Arrays<int>::getDim(array,&n2);
+    cout << n2 << "\n";
+    for(int i = 0; i < n2; i++)
+        Arrays<int>::insert(array,i,i);
+    Arrays<int>::show(array,false);
+    Arrays<int>::reverse(array);
+    Arrays<int>::show(array,false);
+    Arrays<int>::search(array,4,&bval);
+    cout << bval << "\n";
+    Arrays<int>::search(array,54,&bval);
+    cout << bval << "\n";
+    Arrays<int>::show(array,true);
+    Array<int>* array2;
+    Arrays<int>::create_copy(array,&array2);
+    Arrays<int>::show(array2,false);
+    Arrays<int>::init(array2);
+    Arrays<int>::show(array2,false);
+    Arrays<int>::count(array2,0,&cont);
+    cout << cont << "\n";
+
+    //TODO: Examples para Matrix y usando char
+    /*
+    Array<int>* matrix;
+    Arrays<int>::create(&matrix, f,c);
+    Arrays<int>::insert(matrix,2,2,75);
+    Arrays<int>::extract(matrix,2,2,&val);
+    cout << val << "\n";
+*/
+    delete array;
+    //delete matrix;
+    
     return 0;
 }
